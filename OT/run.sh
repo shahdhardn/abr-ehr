29a95f6ec8b8c3b82eae9639a30279473f10b5d6
Mai-CS <m.mai.egy@gmail.com>
2023-02-23 00:49:21 +0400

    replace SGD with AdamW

--- OT/run.sh
+++ OT/run.sh
@@ -1,2 +1,2 @@
 # srun --ntasks=1 --mem=256G --cpus-per-task=16 -p gpu -q gpu-8 --gres=gpu:4 --output=OT-batch-40.out python -W ignore::UserWarning /home/mai.kassem/sources/abr-ehr/OT/OT_train.py
-srun --ntasks=1 --mem=64G --cpus-per-task=16 -p gpu -q gpu-8 --gres=gpu:1 --output=OT-batch-16.out python -W ignore::UserWarning /home/mai.kassem/sources/abr-ehr/OT/OT_train.py
+srun --ntasks=1 --mem=64G --cpus-per-task=16 -p gpu -q gpu-8 --gres=gpu:1 --output=OT-batch-16.out python -W ignore::UserWarning /home/mai.kassem/sources/abr-ehr/OT/OT_train.py --model="MBertLstm"
